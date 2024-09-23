a4303eba64b89fc9aee9cb6ebee2b93dae60b334
Sergio-Bianchi <sergio.bianchi.2007@calvino.edu.it>
2024-09-23 09:06:14 +0200

    Added function print. Started adding function addStudent. Added function addName {child of addStudent} (to check)

--- header.h
+++ header.h
@@ -6,21 +6,27 @@
 #define C_GESTIONECLASSE_HEADER_H
 #include <stdio.h>
 #define OVERBUFFER 10
+#define bckls() printf("\n")
 
 typedef struct {
-    char name[20];
-    char surname[20];
+    char name[21];
+    char surname[21];
     int height;
 
 } student;
 
-int readData(FILE * fp, student * students);
+int readData(FILE * fp);
 int countLines(FILE * fp);
 int strToInt(char * string);
 int mainLoop();
 
 void printHelp();
+void printStudents();
+void addStudent();
 
+void addName();
+void addSurname();
+void addHeight();
 
 void clrscr();
 #endif //C_GESTIONECLASSE_HEADER_H
--- main.c
+++ main.c
@@ -7,11 +7,11 @@
 
 
 int studentsAmount = 0;
+student *students = NULL;
 
 
 int main(int argc, char *argv[]) {
     FILE *fp;
-    student *students = NULL;
     // Exit if no file is provided
     if (argc < 2) {
         printf("Provide the file you want to edit or create\n");
@@ -35,7 +35,7 @@ int main(int argc, char *argv[]) {
         // Assegna uno spazio in memoria a students per tanti studenti quanti presenti nel file, + un buffer predefinito
         studentsAmount = countLines(fp);
         students = malloc((studentsAmount + OVERBUFFER) * sizeof(student));
-        readData(fp, students);
+        readData(fp);
     }
 
     /* Da qui in avanti, possiamo passare al management del tutto */
@@ -52,12 +52,64 @@ void printHelp() {
            "    p       Print current class\n"
            "    w       Save class to file\n"
            "    m       Print this menu\n"
-           "    cl     Clear the screen\n"
+           "    cl      Clear the screen\n"
            "    e [id]  Edit student\n"
-           "    a [id]  Add a student\n"
+           "    a       Add a student\n"
            "    d [id]  Delete a student\n"
            "    q       Exit\n");
 }
+
+/* Stampa la tabella caricata in RAM*/
+void printStudents() {
+    /* Stampa i nomi delle colonne allineati*/
+    printf("%-3s %20s %20s %8s\n\n", "ID", "NAME", "SURNAME", "HEIGHT");
+    /* Stampa tutte le colonne*/
+    for (int i = 0; i < studentsAmount; i++) {
+        printf("%02d) %20s %20s %8d", i, students[i].name, students[i].surname, students[i].height);
+        bckls();
+    }
+}
+
+
+void addName() {
+    char input[20];
+    printf("Name (max 20 c):");
+    fgets(input, 20, stdin);
+
+    /* Se il nome è più corto di tre caratteri, richiama*/
+    if (strlen(input) < 3) {
+        printf("Name length too short");
+        addName();
+    }
+
+    /* Se una delle lettere del nome non è un carattere alfa, richiama*/
+    for (int i = 0; i < 20; ++i) {
+        if (!isalpha(input[i])) {
+            printf("Please use only alpha characters");
+            addName();
+        }
+    }
+
+    strcpy(students[studentsAmount].name, input);
+}
+
+void addStudent() {
+    /* Dichiarazione variabili input. Potrebbe essere ridotta a una*/
+
+    while (1) {
+
+    }
+
+    printf("Surname (max 20 c):");
+    fgets(surname, 20, stdin);
+    printf("Height (cm):");
+    fgets(height, 5, stdin);
+
+
+}
+
+
+/* Ciclo principale dell'applicazione */
 
 int mainLoop() {
     printf("C_GestioneClasse (m for help): ");
@@ -66,7 +118,7 @@ int mainLoop() {
     if (!strcmp(command, "m\n")) printHelp();
     else if (!strcmp(command, "q\n")) return 0;
     else if (!strcmp(command, "cl\n")) clrscr();
-    else if (!strcmp(command, "p\n")) printf("Command PRINT in progress\n");
+    else if (!strcmp(command, "p\n")) printStudents();
     else if (!strcmp(command, "e\n")) printf("Command EDIT called. These is a lot to do here\n");
     else if (!strcmp(command, "w\n")) printf("Command WRITE called. These is a lot to do here\n");
     else if (!strcmp(command, "a\n")) printf("Command ADD called. These is a lot to do here\n");
@@ -98,7 +150,7 @@ int countLines(FILE *fp) {
 }
 
 
-int readData(FILE *fp, student *students) {
+int readData(FILE *fp) {
 
     // Riparti dall'inizio del file (siamo arrivati in fondo dopo aver chiamato la funzione countLines
     rewind(fp);
